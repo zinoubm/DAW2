ee4d7e93c3e3d127c558d3fd755c2f471f9bc3f8
zinoubm <zinne.eddine.2000@gmail.com>
2023-11-07 13:20:58 +0100

    adding backend

---
+++ backend/app/api/endpoints/user.js
@@ -0,0 +1,10 @@
+// post createUser
+// get getUserById
+// get allUsers
+// get currentUser(token) return {informations}
+// - admin
+// - doctor
+// - patient
+
+// put updateUser(token) return {userId}
+// delete deleteUser(token) return {userId}
