e27da21e4e3dd1a1f95f16111e89e4a78533b5d1
lokmanACH <lokmanelhakimachouche0@gmail.com>
2023-11-09 13:10:43 +0100

    crud init

---
+++ backend/app/curd/user.js
@@ -0,0 +1,42 @@
+// CREATE -----------
+
+//it's lokman
+// createUser(userBaseInfo) -> userId
+
+// createAdmin(userId, adminInfo) -> userId
+
+// createDoctor(userId, doctorInfo) -> userId
+
+// createPatient(userId, patientInfo) -> userId
+
+// READ -----------
+
+// getUser(userId) -> object (info about user)
+
+// getUsers() -> [object] (array of objects containing user Info)
+
+// getAdmins() -> [object] (array of objects containing admin Info)
+
+// getDoctors() -> [object] (array of objects containing doctor Info)
+
+// getPatients() -> [object] (array of objects containing patient Info)
+
+// isAdmin(userId) -> boolean
+
+// isDoctor(userId) -> boolean
+
+// isPatient(userId) -> boolean
+
+// UPDATE ----------
+
+// updateUser(newUserBaseInfo) -> userId
+
+// updateAdmin(newAdminInfo) -> userId
+
+// updateDoctor(newDoctorInfo) -> userId
+
+// updatePatient(newPatientInfo) -> userId
+
+// Delete ----------
+
+// deleteUser(userId) -> userId
